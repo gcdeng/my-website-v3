79e40168e3a74ad09b36d4902d247cb5ba7f6aa7
gcdeng <ericdenggc@gmail.com>
2021-07-11 18:15:51 +0800

    add about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import ericDengImgUrl from '@site/static/img/ericdeng.jpg';
+import Intro from './intro.md';
+import { FiGithub, FiLinkedin, FiFacebook, FiTwitter } from 'react-icons/fi';
+import styles from './styles.module.css';
+
+const About = () => {
+  return (
+    <Layout title="Hello">
+      <div className="container margin-vert--lg">
+        <h1>About Eric Deng</h1>
+        <img src={ericDengImgUrl} className={styles.avatar}></img>
+        <div className={styles.icons}>
+          <a href="https://github.com/gcdeng" target="_blank">
+            <FiGithub className={styles.icon} />
+          </a>
+          <a href="https://www.linkedin.com/in/gcdeng/" target="_blank">
+            <FiLinkedin className={styles.icon} />
+          </a>
+          <a href="https://www.facebook.com/ericdenggc" target="_blank">
+            <FiFacebook className={styles.icon} />
+          </a>
+          <a href="https://twitter.com/ericdenggc" target="_blank">
+            <FiTwitter className={styles.icon} />
+          </a>
+        </div>
+        <div className={styles.intro}>
+          <Intro />
+          <a href="mailto:ericdenggc@gmail.com">ericdenggc@gmail.com</a>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
